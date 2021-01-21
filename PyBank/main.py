eaa46fd3c93671f29837475fb396787a97a119f2
Jeannaej <jeannaejy22@gmail.com>
2021-01-20 20:32:59 -0600

    Adding code to import and read the budget_data.csv on for PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,24 @@
 #This challenge is to create a Python script to analyze the financial records, Profit/Losses of a company.
+
+#Importing modules
+
+import os
+import csv
+
+#Path to Resources
+
+budgetData = os.path.join("Resources","budget_data.csv")
+
+#Reading the CSV
+
+with open(budgetData) as csvfile:
+    readBudgetData = csv.reader(csvfile, delimiter= ",")
+    print(readBudgetData)
+    
+    dataheader = next(readBudgetData)
+    print(dataheader)
+
+#For loop printing the csv
+
+    for row in readBudgetData:
+        print(row)
