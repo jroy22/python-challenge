1964f52be6c1d4a422adfde810bbf2e790c1f409
Jeannaej <jeannaejy22@gmail.com>
2021-01-20 23:47:36 -0600

    Added code to import modules and read election data.csv for PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,22 @@
 #This challenge is to help a rural town modernize the counting of their elections.
+
+#Importing modules
+
+import os
+import csv
+
+#Path to Resources
+
+electionData = os.path.join("Resources","election_data.csv")
+
+#Opening election_data.csv
+
+with open(electionData) as csvfile:
+  
+   #Reading the CSV
+
+    readElectionData = csv.reader(csvfile, delimiter= ",")
+    dataheader = next(readElectionData) 
+
+    print(readElectionData)
+    print(dataheader)
