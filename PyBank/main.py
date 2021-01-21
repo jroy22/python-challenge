3f0714b73e989370ab6f67aec009fb3b5706c579
Jeannaej <jeannaejy22@gmail.com>
2021-01-20 21:45:18 -0600

    Added code to find the sum of the profit/losses and the start of the summary table

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,7 +18,23 @@ with open(budgetData) as csvfile:
     dataheader = next(readBudgetData)
     print(dataheader)
 
-#For loop printing the csv
+#Setting up variables
 
+    netTotal = 0
+
+    #For loop printing the csv
+   
     for row in readBudgetData:
+        profitLoss = int(row[1])
         print(row)
+
+
+
+        #Sum of the net total amount of Profit/Losses of the entire period
+
+        netTotal += profitLoss
+    
+    #Printing Summary Table
+    print(f'Financial Analysis')
+    print("-" * 28)
+    print(f'Total: ${netTotal}')
